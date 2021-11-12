f3b35a062d1f9c07f96111cd6f85887e1dab7545
NiharikaSanamsetty <41267506+NiharikaSanamsetty@users.noreply.github.com>
2021-11-12 02:07:40 -0600

    committed

--- controllers/ball.js
+++ controllers/ball.js
@@ -1,66 +1,84 @@
 var ball = require('../models/ball');
 // List of all balls
-exports.ball_list = async function(req, res) {
-    try{ 
-        let balls = await ball.find(); 
-        res.send(balls); 
-    } 
-    catch(err){ 
-        res.status(500); 
-        res.send(`{"error": ${err}}`); 
-    }   
+exports.ball_list = async function (req, res) {
+    try {
+        let balls = await ball.find();
+        res.send(balls);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 // for a specific ball.
-exports.ball_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: ball detail: ' + req.params.id);
+exports.ball_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: ball detail: ' + req.params.id);
 };
 // Handle ball create on POST.
-exports.ball_create_post = async function(req, res) {
+exports.ball_create_post = async function (req, res) {
     console.log(req.body)
     let document = new ball();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
     // {"costumetype":"goat", "cost":12, "size":"large"}
-    document.balltype = req.body.balltype;
-    document.price = req.body.price;
     document.color = req.body.color;
-    try{
-    let result = await document.save();
-    res.send(result);
+    document.material = req.body.material;
+    document.cost = req.body.cost;
+    try {
+        let result = await document.save();
+        res.send(result);
     }
-    catch(err){
-    res.error(500,`{"error": ${err}}`);
+    catch (err) {
+        res.error(500, `{"error": ${err}}`);
     }
-    };
+};
 // Handle ball delete form on DELETE.
-exports.ball_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: ball delete DELETE ' + req.params.id);
+exports.ball_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: ball delete DELETE ' + req.params.id);
 };
 // Handle ball update form on PUT.
-exports.ball_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: ball update PUT' + req.params.id);
-}; 
+exports.ball_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await ball.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.color)
+            toUpdate.color = req.body.color;
+        if (req.body.material) toUpdate.material = req.body.material;
+        if (req.body.cost) toUpdate.cost = req.body.cost;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+    }
+};
 
 // VIEWS
 // Handle a show all view
-exports.ball_view_all_Page = async function(req, res) {
-    try{
-    theball = await ball.find();
-    res.render('ball', { title: 'ball Search Results', results: theball });
+exports.ball_view_all_Page = async function (req, res) {
+    try {
+        theball = await ball.find();
+        res.render('ball', { title: 'ball Search Results', results: theball });
     }
-    catch(err){
-    res.error(500,`{"error": ${err}}`);
+    catch (err) {
+        res.error(500, `{"error": ${err}}`);
     }
-    }; 
-    // for a specific ball 
-exports.ball_detail = async function(req, res) { 
-    console.log("detail"  + req.params.id) 
-    try { 
-        result = await ball.findById( req.params.id) 
-        res.send(result) 
-    } catch (error) { 
-        res.status(500) 
-        res.send(`{"error": document for id ${req.params.id} not found`); 
-    } 
-}; 
+};
+// for a specific ball 
+exports.ball_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await ball.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
+
+
