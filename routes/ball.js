c2204b98ae5178fb655083808e86bcb20859423e
NiharikaSanamsetty <41267506+NiharikaSanamsetty@users.noreply.github.com>
2021-11-17 23:03:33 -0600

    committed

--- routes/ball.js
+++ routes/ball.js
@@ -5,3 +5,12 @@ var router = express.Router();
 /* GET balls */ 
 router.get('/', ball_controlers.ball_view_all_Page ); 
 module.exports = router; 
+/* GET detail ball page */ 
+router.get('/detail', ball_controlers.ball_view_one_Page); 
+/* GET create ball page */ 
+router.get('/create', ball_controlers.ball_create_Page); 
+/* GET create update page */ 
+router.get('/update', ball_controlers.ball_update_Page);
+/* GET create costume page */ 
+router.get('/delete', ball_controlers.ball_delete_Page); 
+ 
